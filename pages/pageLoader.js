eeabab5f9fe4c151d6978fac342a0de21adc3144
SimplyBLGDev <61010688+SimplyBLGDev@users.noreply.github.com>
2020-09-08 16:03:23 -0300

    Pokemon Maps

--- pages/pageLoader.js
+++ pages/pageLoader.js
@@ -3,12 +3,19 @@ loadPage();
 function loadPage() {
     var xhttp = new XMLHttpRequest();
     var content = document.getElementById("Content").innerHTML;
+    var footer = null;
+    if (document.getElementById("Footer") != null) {
+        footer = document.getElementById("Footer").innerHTML;
+    }
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         document.getElementById("base").innerHTML = this.responseText;
         document.getElementById("Content").innerHTML = content;
+        if (footer != null) {
+            document.getElementById("Footer").innerHTML = footer;
+        }
     }
     };
-    xhttp.open("GET", "./pages/globalPage.html", true);
+    xhttp.open("GET", "/pages/globalPage.html", true);
     xhttp.send();
 }
